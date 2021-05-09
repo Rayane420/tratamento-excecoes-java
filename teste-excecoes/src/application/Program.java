472a2818a0e2f5da3ddae2fb3ab174a0ef7cd4cc
Rayane Ribeiro <46855211+Rayane420@users.noreply.github.com>
2021-05-09 09:15:42 -0300

    Add files via upload

---
+++ teste-excecoes/src/application/Program.java
@@ -0,0 +1,40 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entites.Account;
+
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US); //os numeros serão separados por pontos
+		
+		Scanner read = new Scanner(System.in);
+		
+		Account acc = new Account();
+		
+		System.out.println("-----Informe os dados da conta ---------------");
+		System.out.println("Numero: ");
+		acc.setNumber(read.nextInt());
+		
+		System.out.println("Titular: ");
+		read.nextLine();
+		acc.setHolder(read.nextLine());
+		
+		System.out.println("Saldo Inicial: ");
+		acc.setBalance(read.nextDouble());
+		
+		System.out.println("Limite de saque: ");
+		acc.setWithdrawLimit(read.nextDouble());
+		
+		
+		System.out.println("Informe o valor de saque: ");
+		acc.withdraw(read.nextDouble());
+		
+		System.out.println("Saldo atual: R$"+acc.getBalance());
+	}
+
+}                       
